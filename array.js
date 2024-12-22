86d53ce3bedffa80ed372740d0195bc582f94a96
Chinmoy94 <chinmoym66@gmail.com>
2024-12-23 00:30:12 +0530

    js oparetor and function with loops and strings

--- array.js
+++ array.js
@@ -1,4 +1,194 @@
-/*let marks=[97,82,67,78,90];
+/* artimetic Operators
+let a = 5;
+let b = 2;
+console.log("a=",a,"& b=",b);
+console.log("a+b=", a+b);
+console.log("a-b=", a-b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);
+console.log("a%b=",a%b);
+console.log("a**b=",a**b);
+//unary operator
+a++;
+console.log("a++=",a++);
+console.log("a=",a);
+a--;
+console.log("a--=",a--);
+++a;
+console.log("a=",a);
+//Asignment Operators
+a+=4;
+console.log("a=",a);
+a**=4;
+console.log("a=",a);
+//comparison opperators
+console.log("5>=3",a>=b);
+//Logical operator
+let cond1 =a<b;
+let cond2 = a===5;
+console.log("!(5<3)= ",!(a>b), ! (a===5));
+//conditional Statement
+/*let age=25;
+if(age>=18){
+    console.log("you can vote");
+}
+let mode="blue";
+let color;
+if(mode==="dark"){
+    color="black";
+}
+else{
+    color="white";
+}
+console.log(color);
+let age=18;
+if(age>=18){
+    console.log("vote");
+}else{
+    console.log("not vote");
+}
+// odd even
+let num=97;
+if(num%2===0){
+    console.log(num,"is even");
+}else{
+    console.log(num,"is odd");
+}
+// syntax->
+
+let room ="siver";
+let colour;
+if(room==="dark"){
+    colour="black";
+} else if(room==="blue"){
+    colour="blue";
+} else if(room==="pink"){
+    colour="pink";
+}else{
+    colour="white";
+}
+console.log(colour);*/
+//Ternary operators
+/*let age = 25;
+let result = age>=18 ? console.log ("adult") : console.log("not adult");
+alert("hello chinmoy");
+let name=prompt("box");
+console.log("name");*/
+/*let num =prompt("enter a number");
+if(num%5===0){
+    console.log(num, "number is multiple of 5");
+}else{
+    console.log(num,"num is not multiple of 5");
+}*/
+/*let score=95;
+let grade;
+if(score>=90 && score<=100){
+    grade=" Grade";
+}
+/* console.log(" grade :", grade);
+
+    let score=75;
+    let grade;
+    if(score>=70 &&  score<=90){
+        grade="B Grade";
+    }
+
+    console.log("grade:",grade);*/
+/*const profile={
+    username:"@chinmoy",
+    isFollow:false,
+    followers:123,
+    following:123,
+};
+console.log("profile")
+*/
+/*for(let count=1; count<=5; count++){
+    console.log("chinmoy");
+}*/
+// calculate sum of 1to5
+/*let sum=0;
+let n=100;
+for(let i=1; i<=n; i++){
+    sum=sum+i;
+}
+console.log("sum=",sum,);*/
+/*for(let i=10; i<=25; i++){
+    console.log(i);
+}*/
+/*let i=5;
+while(i<=10){
+    console.log(i);
+    i++;
+}*/
+/*let i=1;
+do{
+    console.log(i);
+    i++;
+} while(i<=10);*/
+ /*let str="chinmoy";
+ let size=0;
+ for(let i of str){
+    console.log("i=",i);
+    size++;
+ }
+ console.log("string size=",size);*/
+ /*let student={
+    name:"Chinmoy Manna",
+    age:26,
+    percentage:7.6,
+    isPass:true,
+ };
+ for(let key in student){
+    console.log("key=",key,"value=", student[key]);
+ }*/
+//practice
+/*for(let num=0;num<=100;num++){
+    if(num%2!==0){
+        console.log(num);
+    }
+}*/
+//practice 2
+/*let gameNum=25;
+let userNum=prompt("Guess the game number:");
+while(usernum!=gamNum){
+    userNum=prompt("you entered wrong number,Guess agian:");
+}
+console.log("congatulations, you enterd the right number");
+*/
+//strings
+/*let str="chinmoy";
+console.log(str[4]);*/
+//template literals
+//let sentence=`This is a book`;
+//console.log(typeof sentence);
+/*let obj={
+    item:"pen",
+    price:10,
+};
+let output=`the cost of ${obj.item} is ${obj.price} rupess`;
+console.log("the cost of",obj.item,"is",obj.price, "rupees");*/
+/*let str="Chinmoy";
+let newStr=str.toUpperCase();
+console.log(str);
+console.log(newStr);*/
+/*let str="AARUSH";
+str=str.toLowerCase();
+console.log(str);*/
+/*let str="     chinmoy manan   ";
+console.log(str.trim());*/
+/*let str="chinmoy";
+console.log(str.slice(1,5));*/
+/*let str1 = "chinmoy";
+let str2 ="manna";
+let res ="my name is" + str1 +str2;
+console.log(res);*/
+let str="chinmoy";
+console.log(str.replace(i,o));
+let fullName = prompt("enter your fullname without spaces");
+let userName= "@"+fullName + fullName.length;
+console.log(userName);
+
+ /*let marks=[97,82,67,78,90];
 console.log(marks);
 console.log(marks);
 console.log(marks. length);
